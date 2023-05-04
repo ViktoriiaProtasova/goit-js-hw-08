94260efeb4dab4ae8f46e70d1e450b1287f95292
Viktoriia Protasova <viktorka.protasova@gmail.com>
2023-05-04 20:35:58 +0300

    Update 01-gallery files

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+// Change code below this line
+const galleryListEl = document.querySelector('.gallery');
+
+function createGallery() {
+  const galleryItemsMarkUp = galleryItems
+    .map(
+      item =>
+        `<li class="gallery__item" style="list-style: none">
+   <a class="gallery__link" href="${item.original}">
+      <img class="gallery__image" src="${item.preview}" alt="${item.description}" title="${item.description}" />
+   </a>
+</li>`
+    )
+    .join('');
+
+  galleryListEl.insertAdjacentHTML('beforeend', galleryItemsMarkUp);
+}
+
+createGallery();
+
+new SimpleLightbox('.gallery__link', {
+  captionDelay: 250,
+  animationSlide: false,
+});
