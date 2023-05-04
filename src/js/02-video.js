872e566d5fdf9f52826e2901a301ca0e937b83d6
Viktoriia Protasova <viktorka.protasova@gmail.com>
2023-05-04 23:30:19 +0300

    Update 02-video files

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const onTimeupdate = throttle(
+  function (event) {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(event));
+  },
+  1000,
+  { trailing: false }
+);
+
+try {
+  const savedSettings = localStorage.getItem('videoplayer-current-time');
+  const parsedSettings = JSON.parse(savedSettings);
+
+  player
+    .setCurrentTime(parsedSettings.seconds)
+    .then(function (seconds) {})
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          break;
+      }
+    });
+} catch (error) {
+  console.error('An error occurred:', error);
+}
+
+player.on('timeupdate', onTimeupdate);
