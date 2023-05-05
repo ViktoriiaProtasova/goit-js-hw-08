2dab180ec8d33d3522f7989fc85ca55a8c1e6b6c
Viktoriia Protasova <viktorka.protasova@gmail.com>
2023-05-05 22:34:05 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,52 @@
+import throttle from 'lodash.throttle';
+import debounce from 'lodash.debounce';
 
+// Отримання відомостей форми та елементів введення
+const form = document.querySelector('form');
+const emailInput = document.querySelector('[type="email"]');
+const messageInput = document.querySelector('[name="message"]');
+
+// Функція для збереження даних у локальне сховище
+const saveFormState = throttle(function () {
+  const state = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(state));
+}, 500);
+
+// Додати обробник події для введення
+form.addEventListener(
+  'input',
+  debounce(event => {
+    saveFormState();
+  }, 1500)
+);
+
+// Відновити стан форми після завантаження сторінки
+window.addEventListener('load', function () {
+  const state = localStorage.getItem('feedback-form-state');
+  if (state) {
+    const { email, message } = JSON.parse(state);
+    emailInput.value = email;
+    messageInput.value = message;
+  }
+});
+
+// Очистити сховище і поля форми gід час сабміту форми
+form.addEventListener('submit', submitedForm);
+
+function submitedForm(event) {
+  event.preventDefault();
+  const state = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+
+  localStorage.removeItem('feedback-form-state');
+
+  console.log(state);
+
+  emailInput.value = '';
+  messageInput.value = '';
+}
